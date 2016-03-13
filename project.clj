67517c20fa262227adf0f3f2e65b147f9174cac4
Victor Neo <icyisamu@gmail.com>
2016-03-13 15:53:50 +0800

    Add logging capabilities for dev and prod

--- project.clj
+++ project.clj
@@ -3,13 +3,21 @@
   :url "http://github.com/victorneo/composure"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.0"]
-                 [ring/ring-defaults "0.2.0"]
-                 [ring/ring-jetty-adapter "1.2.1"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [cheshire "5.5.0"]
+                 [compojure "1.5.0"]
+                 [cprop "0.1.6"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
                  [metosin/compojure-api "1.0.1"]
                  [mount "0.1.10"]
-                 [cprop "0.1.6"]]
+                 [org.slf4j/slf4j-log4j12 "1.7.18"]
+                 [ring/ring-defaults "0.2.0"]
+                 [ring/ring-jetty-adapter "1.2.1"]]
+  :ring {:init composure.core/init-app
+         :handler composure.handler/app}
   :main composure.core
   :resource-paths ["env/dev/resources"]
   :plugins [[lein-ring "0.9.7"]
@@ -18,5 +26,6 @@
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}
+   :uberjar {:resource-paths ["env/prod/resources"]}
    :test {:resource-paths ["env/dev/resources" "env/test/resources"]}
    :repl {:resource-paths ["env/dev/resources" "env/test/resources"]}})
--- src/composure/core.clj
+++ src/composure/core.clj
@@ -1,11 +1,15 @@
 (ns composure.core
-  (require [composure.config :refer [env]]
+  (require [clojure.tools.logging :as log]
+           [composure.config :refer [env]]
            [composure.handler :refer [app]]
            [mount.core :as mount]
            [ring.adapter.jetty :as jetty])
   (:gen-class))
 
+(defn init-app []
+  (mount/start))
 
 (defn -main [& args]
- (mount/start)
- (jetty/run-jetty app {:port 3000}))
+ (init-app)
+ (log/info (str "Application will be started on port " (:port env)))
+ (jetty/run-jetty app {:port (:port env)}))
